3a61b5621e27061feff59a92bf2ff562731689cf
Tetiana-UA <izidaxm85@gmail.com>
2023-12-10 00:02:39 +0100

    cart-10

--- src/js/cart-localestorage.js
+++ src/js/cart-localestorage.js
@@ -1,45 +1,69 @@
-export { KEY_CART, cartArr, addToCart, deleteFromCart,  findProduct };
+export { KEY_CART, addToCart, cartArr, deleteFromCart, findProduct }
+import { updateCartNumber } from './header'
 
+const refs = {
+  cartEmpty: document.querySelector('.js-empty-cart'),
+  cartFull: document.querySelector('.cart-full')
+}
 
 //Створюємо ключ для localeStorage
-const KEY_CART = 'cart';
+const KEY_CART = 'cart'
 //Створюємо пустий масив для доданих продуктів або наповнюємо його з localeStorage, якщо там вже щось є
-const cartArr = JSON.parse(localStorage.getItem(KEY_CART)) ?? [];
+const cartArr = JSON.parse(localStorage.getItem(KEY_CART)) ?? []
 //Оголосимо зміну для запису id вибраного продукту
-let productId;
+let productId
 
 //Функція для додавання товарів в LocaleStorage по кліку на кнопку корзини
 function addToCart(evt, arr) {
+  console.log('addToCart')
   //При кліку на кнопку шукаємо потрібний продукт за id, викликаючи функцію findProduct
-  const product = findProduct(evt.target, arr);
-  cartArr.push(product);
-  localStorage.setItem(KEY_CART, JSON.stringify(cartArr));
-  console.log(product);
+  const product = findProduct(evt.target, arr)
+  cartArr.push(product)
+  localStorage.setItem(KEY_CART, JSON.stringify(cartArr))
+  console.log(product)
+
+  updateCartNumber()
 }
 
 //Функція пошуку необхідного продукту за id в масиві,який надходить з серверу (викликається всередині addToCart)
 function findProduct(elem, arr) {
-  productId = elem.dataset._id;
-  return arr.find(({ _id }) => _id === productId);
-} 
+  productId = elem.dataset._id
+  return arr.find(({ _id }) => _id === productId)
+}
 
 
-//???????????????????????????????????????????????????????????????????
+
 //Функція для видалення товарів з LocaleStorage по кліку на кнопку delete
-function deleteFromCart(evt, cartArr) {
-  //Перебираємо масив корзини, шукаємо індекс продукту з вибраним id і видаляємо його з масиву за індексом
-  for (let i= 0; i < cartArr.length; i += 1 ) {
-    productId = evt.target.dataset._id;
-    if (cartArr[i]._id === productId) {
-      cartArr.splice(i, 1); 
-      localStorage.setItem(KEY_CART, JSON.stringify(cartArr)); 
+function deleteFromCart(cartArr, btn) {
+  try {
+    const selectedProduct = btn.closest('.selectedProduct').dataset.id
+    console.log(`deleted ${selectedProduct}`)
+    //Перебираємо масив корзини, шукаємо індекс продукту з вибраним id і видаляємо його з масиву за індексом
+    if (selectedProduct) {
+      for (let i = 0; i < cartArr.length; i++) {
+        if (cartArr[i]._id === selectedProduct) {
+          cartArr.splice(i, 1)
+          localStorage.setItem(KEY_CART, JSON.stringify(cartArr))
+          updateCartNumber()
+        }
+      }
+    }
+
+
+  } catch (error) {
+    console.log(error)
+  } finally {
+    if (cartArr.length === 0) {
+      localStorage.removeItem(KEY_CART)
+      refs.cartEmpty.style.display = 'flex'
+      refs.cartFull.style.display = 'none'
+      document.querySelector('.js-cart-numbers').innerHTML = 0
+      document.querySelector('#cart-count').innerHTML = 0
+    }
   }
-}
+
 
 }
-
-//console.log(evt.target);
-//console.log(deleteFromCart(evt, cartArr));
 
 
 
