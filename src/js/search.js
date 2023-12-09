af82b0dfaf62b3096b414e32acf1a342d86c9287
Olvet7 <volya.maslova@gmail.com>
2023-12-09 02:42:11 +0200

    search (need to fix)

--- src/js/search.js
+++ src/js/search.js
@@ -1,56 +1,54 @@
-// import { Notify } from "notiflix";
-// import {
-//     foodInfo,
-//     fetchAndRender,
-//     fetchFoodCategory,
-//     getCategoriesFromLS,
-//     KEY_CATEGORY,
-//     renderFoodItems,
-//   } from './products';
+import { Notify } from "notiflix";
+import {
+    foodInfo,
+    fetchAndRender,
+    fetchFoodCategory,
+    getCategoriesFromLS,
+    KEY_CATEGORY,
+    renderFoodItems,
+  } from './products';
 // import { Input } from "postcss";
 
-// const form = document.getElementById('myForm');
+const form = document.querySelector('#search');
 // const input = document.querySelector('.search-input')
-// let timeoutId; // Змінна для зберігання ідентифікатора таймаута
+let timeoutId; // Змінна для зберігання ідентифікатора таймаута
 
-// // Додавання слухача подій до форми
-// input.addEventListener('input', handleInputOrSubmit);
-// form.addEventListener('submit', handleInputOrSubmit);
+// Додавання слухача подій до форми
 
-// function handleInputOrSubmit(event) {
+form.addEventListener('input', handleInputOrSubmit);
+form.addEventListener('submit', handleInputOrSubmit);
 
-//   event.preventDefault();
+function handleInputOrSubmit(event) {
+  event.preventDefault();
+  const keyword = form.elements.search.value.trim();
 
-//   const keyword = event.target.elements.searchInput.value;
-//   console.log(keyword);
+  // Визначення типу події
+  if (event.type === 'input') {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      pushAndRender(keyword);
+    }, 1000);
+  } else if (event.type === 'submit') {
+    pushAndRender(keyword);
+  }
 
-//   // Визначення типу події
-//   if (event.type === 'input') {
-//     clearTimeout(timeoutId);
-//     timeoutId = setTimeout(() => {
-//       console.log('Input event after 0.3 second delay');
-//     }, 1000);
-//   } else if (event.type === 'submit') {
-//     console.log('Submit event triggered');
-//   }
+}
 
-//   if (storedData) {
-//     try {
-//         // Розпакувати JSON-рядок у Javascript-об'єкт
-//         const parsedData = JSON.parse(storedData);
-//         // Змінити тільки потрібну властивість (наприклад, keyword)
-//         parsedData.keyword = `${keyword}`;
+const storedData = localStorage.getItem('filter');
 
-//         // Зберегти оновлений об'єкт назад в localStorage
-//         localStorage.setItem('filter', JSON.stringify(parsedData));
-//     } catch (error) {
-//         console.error('Error updating localStorage:', error);
-//     }
-// }
-//     fetchAndRender();
-// }
+function pushAndRender(keyword) {
+  if (storedData) {
+    try {
+      // Розпакувати JSON-рядок у Javascript-об'єкт
+      const parsedData = JSON.parse(storedData);
+      // Змінити тільки потрібну властивість (наприклад, keyword)
+      parsedData.keyword = `${keyword}`;
 
-// // Слухач на форму
-// // По інпуту/сабміту = додаємо в ЛокалСторадж
-// // виклик handleSubmit
-// // fetchAndRender
+      // Зберегти оновлений об'єкт назад в localStorage
+      localStorage.setItem('filter', JSON.stringify(parsedData));
+    } catch (error) {
+      console.error('Error updating localStorage:', error);
+    }
+  }
+  fetchAndRender();
+}
