feb0296c1e63e3fa51ed43fcb21833b8757b6128
RAGE1712 <137414196+RAGE1712@users.noreply.github.com>
2023-12-10 04:08:32 +0200

    Footer subscription

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -7,7 +7,7 @@ const FILTER = 'filter';
 const refs = {
   pagination: document.querySelector('.tui-pagination'),
   list: document.querySelector('.product-list'),
-  select: document.querySelector('.select'),
+  select: document.querySelector('.select__body'),
 };
 
 const container = document.getElementById('pagination');
